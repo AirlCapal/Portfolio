3e07839610932f05547baa7c210b6ff6731d51c7
Airl Capal <cjaecapal18@gmail.com>
2023-07-25 11:44:40 +0800

    Initial commit: Bootstrap Integration

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
