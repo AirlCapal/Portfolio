45ca6e9309e530e840461ea6b60417f2cf7be9a4
Airl Capal <cjaecapal18@gmail.com>
2023-07-25 11:25:42 +0800

    Initial commit: Setup React app with routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
-import './App.css';
-import { BrowserRouter as Router, Routes ,Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 
@@ -9,8 +8,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path ="/" element={<Home/>}/>
-          <Route path ="/" element={<Projects/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </Router>
     </div>
